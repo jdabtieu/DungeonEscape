14d5d42ff7319782e6e75c0271810db10859a7c5
Jonathan Wu <jonathan.wu3@outlook.com>
2021-12-20 22:43:50 -0500

    add picking up weapons

---
+++ src/com/jdabtieu/DungeonEscape/map/GroundWeapon.java
@@ -0,0 +1,28 @@
+package com.jdabtieu.DungeonEscape.map;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class GroundWeapon extends Sensor {
+
+    public GroundWeapon(Runnable action) {
+        super(action);
+        setLayout(null);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setBackground(Ground.COLOR);
+        try {
+            JLabel icon = new JLabel(new ImageIcon(ImageIO.read(new File("assets/weapon.png"))));
+            icon.setBounds(0, 0, 20, 20);
+            add(icon);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
