e8b2f3ef8e78d7f08c3c81c8791df2a517b7010d
Jonathan Wu <jonathan.wu3@outlook.com>
2021-12-26 20:57:14 -0500

    testing game show panel

--- src/Testing.java
+++ src/Testing.java
@@ -1,10 +1,12 @@
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.util.ArrayList;
 
-import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import com.jdabtieu.DungeonEscape.core.Window;
@@ -38,16 +40,39 @@ public class Testing extends JFrame {
         setBounds(100, 100, Window.WIDTH, Window.HEIGHT);
         getContentPane().setLayout(null);
         contentPane = new JPanel();
-        contentPane.setBounds(0, 0, Window.WIDTH, Window.HEIGHT);
+        contentPane.setBounds(0, 0, Window.WIDTH / 2, 200);
         getContentPane().add(contentPane);
         contentPane.setLayout(null);
         contentPane.setBackground(Color.GREEN);
         
-        Text txt = new Text("Interview Room", 100, 100);
+        String title = "Guess the Phrase (80s Song Lyrics)";
+        String ans = "vector";
+        
+        Text txt = new Text(title, 0, 10);
         txt.setHorizontalAlignment(SwingConstants.CENTER);
-        txt.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        txt.setBounds(100, 100, 148, 40);
+        txt.setBounds(0, 10, contentPane.getWidth(), 40);
         txt.setFont(new Font("Sitka Text", Font.BOLD, 16));
         contentPane.add(txt);
+        
+        ArrayList<JTextField> letters = new ArrayList<>();
+        for (int i = 0; i < ans.length(); i++) {
+            if (ans.charAt(i) == ' ') continue;
+            JTextField f = new JTextField();
+            f.setBounds((10 + 40 * i) % contentPane.getWidth(), 60 * ((10 + 40 * i) / contentPane.getWidth() + 1), 30, 30);
+            letters.add(f);
+            contentPane.add(f);
+        }
+        
+        ans = ans.replace(" ", "");
+        
+        JButton submit = new JButton("Submit");
+        submit.setBounds(contentPane.getWidth() / 2 - 50, 160, 100, 30);
+        contentPane.add(submit);
+        submit.addActionListener(e -> {
+            synchronized(this) {
+                notify();
+            }
+        });
+        
     }
 }
