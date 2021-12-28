841dff58d7ec93e647ceafda30ab463a6a21f04a
Jonathan Wu <jonathan.wu3@outlook.com>
2021-12-27 23:30:42 -0500

    fix

--- src/com/jdabtieu/DungeonEscape/core/Fonts.java
+++ src/com/jdabtieu/DungeonEscape/core/Fonts.java
@@ -7,7 +7,7 @@ public class Fonts {
     public static final Font TITLE = new Font("Sitka Text", Font.BOLD, 16);
     public static final Font SUBTITLE = new Font("Sitka Text", Font.BOLD, 16);
     public static final Font STD_PARA = new Font("Tahoma", Font.PLAIN, 12);
-    public static final Font INVENTORY = new Font("Tahoma", Font.PLAIN, 10);
+    public static final Font INVENTORY = new Font("Tahoma", Font.PLAIN, 11);
     public static final Font CLOSE_BTN = new Font("Tahoma", Font.PLAIN, 10);
     public static final Font MONO = new Font("Consolas", Font.PLAIN, 14);
     private Fonts() {}
