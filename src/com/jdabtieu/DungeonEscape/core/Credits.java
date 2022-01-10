49a99c11bfb9be1afba633ce2b7933cadf2fd04e
Jonathan Wu <jonathan.wu3@outlook.com>
2022-01-10 10:50:09 -0500

    update credits

--- src/com/jdabtieu/DungeonEscape/core/Credits.java
+++ src/com/jdabtieu/DungeonEscape/core/Credits.java
@@ -1,7 +1,10 @@
 package com.jdabtieu.DungeonEscape.core;
 
 import java.awt.Color;
+import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.jdabtieu.DungeonEscape.Main;
@@ -27,11 +30,13 @@ public class Credits extends JPanel {
         // must update high score before computing credits text
         Main.getPlayer().updateHighScore();
         final String[] creds = {
-                "Congratulations! You escaped the Dungeon!",
+                "",
+                "",
+                "",
+                "",
                 "",
                 "Final Score: " + Main.getPlayer().score(),
                 "High Score: " + Player.getHighScore(),
-                "",
                 "",
                 "",
                 "Credits",
@@ -54,20 +59,21 @@ public class Credits extends JPanel {
         final Text[] lines = new Text[creds.length];
         final int totalTime = 38000; // 38s animation time
         final int lineTime = totalTime / (Window.HEIGHT + 50 * creds.length); // time per line
+        final JLabel logo = new JLabel(new ImageIcon(Toolkit.getDefaultToolkit().getImage("assets/titleTextInv.png")));
+        logo.setBounds((Window.WIDTH - 834) / 2, Window.HEIGHT, 834, 65);
+        add(logo, 10000, 0);
         
         for (int i = 0; i < creds.length; i++) {
             // convert each line of text into GUI component
             lines[i] = new Text(creds[i], 100, Window.HEIGHT + 50 * i);
-            lines[i].setForeground(Color.white);
+            lines[i].setForeground(Color.WHITE);
             lines[i].setFont(Fonts.TITLE);
             add(lines[i]);
         }
         
-        // these two lines should be in large font
-        for (int e : new int[] {0, 7}) {
-            lines[e].setFont(Fonts.LARGE);
-            lines[e].setBounds(100, Window.HEIGHT + 50 * e, 1000, 32);
-        }
+        // these credits text should be in large font
+        lines[9].setFont(Fonts.LARGE);
+        lines[9].setBounds(100, Window.HEIGHT + 50 * 9, 1000, 32);
         
         // start the audio
         Music.initAudio("win.wav", false);
@@ -78,10 +84,12 @@ public class Credits extends JPanel {
                 Main.safeSleep((int) (lineTime - System.currentTimeMillis() + currTime));
             } catch (IllegalArgumentException e) {} // lag, that's fine
             currTime = System.currentTimeMillis();
+            
             // move each item down the screen
             for (Text t : lines) {
                 t.setLocation(100, t.getY() - 1);
             }
+            logo.setLocation(logo.getX(), logo.getY() - 1);
         }
         Main.safeSleep(1000);
     }
